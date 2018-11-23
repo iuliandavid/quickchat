794c94a759e457e37eb1d8a38f7086ad75ca7f4a
Iulian David <david.iulian@gmail.com>
2018-11-23 12:00:06 +0200

    Initial Commit

---
+++ QuickChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickChat
+//
+//  Created by iulian david on 11/23/18.
+//  Copyright © 2018 iulian david. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
