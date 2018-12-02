4d6a4e48d9c3f70e018d5084e9065a951328897d
Iulian David <david.iulian@gmail.com>
2018-12-02 19:46:54 +0200

    avatar retrieval

--- QuickChat/Utils/BackendlessUtils.swift
+++ QuickChat/Utils/BackendlessUtils.swift
@@ -19,31 +19,54 @@ struct BackendlessUtils {
     /// - parameter result: callback containing a image or nil
     static func getAvatarFromURL(url: String, result: @escaping (_ image: UIImage?) -> Void) {
         
-        guard let url = URL(string: url) else {
+        guard let url = URL(string: url), let userToken = backendless?.userService.currentUser.getToken() else {
             return
         }
         
         let downloadQueue = DispatchQueue.init(label: "imageDownloadQueue")
-        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.setValue("\(userToken)", forHTTPHeaderField: "user-token")
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
+            
+            guard error == nil else {
+                ProgressHUD.showError("There was an error retrieving avatar: \(error!.localizedDescription)")
+                DispatchQueue.main.async {
+                    result(nil)
+                }
+                return
+            }
+            
+            guard let responseCode = (response as? HTTPURLResponse)?.statusCode,
+            200 ... 299 ~= responseCode else {
+                ProgressHUD.showError("There was an error retrieving avatar, status code: \((response as? HTTPURLResponse)?.statusCode ?? 0)")
+                DispatchQueue.main.async {
+                    result(nil)
+                }
+                return
+            }
+            if let data = data,
+                let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    result(image)
+                }
+            }
+            
+            
+        })
         downloadQueue.async {
-            guard let data = try? Data(contentsOf: url) else {
-                return
-            }
-            
-            guard let image = UIImage(data: data) else {
-                return
-            }
-            
-            DispatchQueue.main.async {
-                result(image)
-            }
+            dataTask.resume()
         }
+        
     }
     
     /// A helper function to retrieve the image from Backendless server
-    /// - parameter user: the url on the server
-    /// - parameter result: callback containing a image or nil
-    static func uploadAvatar(uploadDirectory: String = "/upload/", image: UIImage, result: @escaping(_ imageLink: String?, _ fault: Fault?) -> Void) {
+    /// - parameter uploadDirectory: the directory in which the image should be stored
+    /// - parameter image: the image to upload
+    /// - parameter result: callback containing a link or nil
+    static func uploadAvatar(uploadDirectory: String = "/upload/",
+                             image: UIImage,
+                             result: @escaping(_ imageLink: String?, _ fault: Fault?) -> Void) {
         
         let data = image.jpegData(compressionQuality: 1)
         let filePathName = "\(uploadDirectory)\(Date().formatted()).jpg"
